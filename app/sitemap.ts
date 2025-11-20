64d2820fbfac685afcd8f1a610a36b6292126982
MeesEgberts <mail@meesegberts.nl>
2025-11-20 19:23:45 +0100

    fixed wrong date

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export const baseUrl = "https://meesegberts.nl";
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+    lastModified: new Date(post.metadata.publishedAt),
   }));
 
   let routes = ["", "/blog"].map((route) => ({
